dcc67a491575ab7256187a2a0d85857302d04693
Luis Maestre <luisaugustomaestre@gmail.com>
2024-02-09 14:50:28 -0400

    feat: se agrega clase inicial route para prices

---
+++ src/prices/infrastructure/inbound/price-router.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+// import { HealthController } from "./health-controller";
+
+const priceRouter = express.Router();
+
+// const healthController = new HealthController();
+
+priceRouter.get("/",);
+priceRouter.post("/",);
+
+export { priceRouter };
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import express, { Application } from "express";
 import { config } from "./common/config";
 import { healthRouter } from "./health/health-router";
 import { criptoRouter } from "./criptos/infrastructure/inbound/cripto-router";
+import { priceRouter } from "./prices/infrastructure/inbound/price-router";
 
 dotEnvConfig();
 const { port } = config.server;
@@ -23,13 +24,15 @@ export class Server {
   }
 
   async start(): Promise<void> {
-    await this.dbConnection(); // Call to connect before listening
+    await this.dbConnection();
 
     // Middlewares
     this.app.use(bodyParser.json());
+
     // Rutas de mi aplicación
-    this.app.use("/v1/flixxo-app/cripto", criptoRouter);
     this.app.use("/api/v1/flixxo-app/health", healthRouter);
+    this.app.use("/v1/flixxo-app/cripto", criptoRouter);
+    this.app.use("/api/v1/flixxo-app/price", priceRouter);
 
     await this.app.listen(this.port, async () => {
       console.log(`[APP] - Starting application on port ${this.port}`);
